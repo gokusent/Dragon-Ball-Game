2d9c5b41a048484b8daa1b53f9fe369b6978476a
gokusent <bereck_ssj@outlook.com>
2025-05-31 20:43:21 +0200

    Mejorar comentarios en Dockerfile y actualizar orígenes permitidos en cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://backend.onrender.com', 'http://localhost:3000'],
+    'allowed_origins' => ['https://dragon-ball-game-1-ppgv.onrender.com', 'https://backend.onrender.com', 'http://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
