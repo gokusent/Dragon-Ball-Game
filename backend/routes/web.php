166519bbd71ff10d8d04470fe65ccbf6134d690e
gokusent <bereck_ssj@outlook.com>
2025-05-31 20:55:38 +0200

    Mejorar comentarios en Dockerfile y agregar ruta para acceder al log de depuración

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-log', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
