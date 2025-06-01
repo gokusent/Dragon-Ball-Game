cd250b490fa98a21de2cd7dacbfc2348be54d9cf
gokusent <bereck_ssj@outlook.com>
2025-06-01 02:09:05 +0200

    Cambios en index.php para usar HTTP kernel en los request handling

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,21 +3,31 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Modo mantenimiento
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Bootstrap
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Crear kernel HTTP
+$kernel = $app->make(Kernel::class);
+
+// Capturar petición y manejar respuesta
+$request = Request::capture();
+$response = $kernel->handle($request);
+
+// Enviar respuesta al cliente
+$response->send();
+
+// Terminar kernel para cerrar tareas pendientes
+$kernel->terminate($request, $response);
